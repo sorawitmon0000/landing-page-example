19f0675e0eb6037ce86e79bf5744b5e81b29802f
sorawitmon0000 <sorawitmon000@gmail.com>
2024-09-19 15:47:53 +0700

    add humberger menu

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,13 @@
+'use client'
 import nextLogo from '@/assets/next-js.svg'
 import Image from "next/image";
+import { useState } from 'react';
+
 export default function Navbar(){
+    const [isOpen,setIsOpen] = useState(false);
+    const toggleMenu = () =>{
+        setIsOpen(!isOpen);
+    }
     return(
         <nav className="container relative mx-auto p-5">
             <div className="flex item-center justify-between">
@@ -16,7 +23,32 @@ export default function Navbar(){
                     <button>Login</button>
                     <button className="p-3 text-black bg-white rounded-lg border-2 border-slate-800 hover:text-white hover:bg-slate-800 hover:border-slate-800">Sign Up</button>
                 </div>
+                <div className='md:hidden'>
+                    <button className='text-black' onClick={toggleMenu}>
+                        <svg
+                            fill='none'
+                            stroke='currentcolor'
+                            stroke-linecap='round'
+                            stroke-linejoin='round'
+                            stroke-width='2'
+                            viewBox='0 0 24 24'
+                            className='w-6 h-6'
+                        >
+                            <path d='M4 6h16M4 12h16M4 18h16'></path>
+                        </svg>
+                    </button>
+                </div>
             </div>
+            {isOpen ? (
+                    <ul className='flex flex-col justify-center items-center space-y-5 bg-slate-200 mt-5 py-5 lg:hidden'>
+                        <li><a href='#' className="text-md font-bold hover:text-cyan-400">sample</a></li>
+                        <li><a href='#' className="text-md font-bold hover:text-cyan-400">sample</a></li>
+                        <li><a href='#' className="text-md font-bold hover:text-cyan-400">sample</a></li>
+                        <li><a href='#' className="text-md font-bold hover:text-cyan-400">Log in</a></li>
+                        <li><a href='#' className="text-md font-bold hover:text-cyan-400">Sign up</a></li>
+                    </ul>
+                    ): null   
+            }
         </nav>
     );
 }
